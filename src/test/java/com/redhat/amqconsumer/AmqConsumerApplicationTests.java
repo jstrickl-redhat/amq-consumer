23ef2302a5d2779faa56a50ccb42beeaef7f8dba
Strickland, Joe <Stricj2@LabCorp.com>
2021-12-03 10:25:14 -0600

    init

---
+++ src/test/java/com/redhat/amqconsumer/AmqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redhat.amqconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
