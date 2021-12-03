f1eb8eb13df23c320d7c5f08b082c57d84b99f25
Strickland, Joe <Stricj2@LabCorp.com>
2021-12-03 12:35:28 -0600

    enable jms

--- src/main/java/com/redhat/amqconsumer/AmqConsumerApplication.java
+++ src/main/java/com/redhat/amqconsumer/AmqConsumerApplication.java
@@ -2,8 +2,10 @@ package com.redhat.amqconsumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class AmqConsumerApplication {
 
     public static void main(String[] args) {
