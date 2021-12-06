862389ef11e69a53c545c4ee8152a71785bbe1ce
Strickland, Joe <Stricj2@LabCorp.com>
2021-12-06 10:39:39 -0600

    update address and queue

--- src/main/java/com/redhat/amqconsumer/Consumer.java
+++ src/main/java/com/redhat/amqconsumer/Consumer.java
@@ -2,9 +2,7 @@ package com.redhat.amqconsumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
-import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
